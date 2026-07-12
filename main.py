3d54f90687179a9851ca316db60f140a5a630c71
Hamidreza Bakhshi <hamidrezabakhshi8118@gmail.com>
2026-07-12 22:27:09 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI  , Query , status , HTTPException , Path , Form  
+from fastapi.responses import JSONResponse
+from typing import Annotated
+import random
+
+
+app=FastAPI()
+
+
+names=[
+    {"id":1,"name":"ali"},
+    {"id":2,"name":"ahmad"},
+    {"id":3,"name":"hossein"},
+    {"id":4,"name":"reza"},
+    {"id":5,"name":"mohsen"},
+]
+
+
+@app.get("/names")
+def get_names(q : str | None = Query(alias="search",default=None , max_length=50)):
+    if q :
+        return JSONResponse(content=[item for item in names if item['name'] == q ])
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+
+
+@app.get("/names/{name_id}")
+def get_name_by_id(name_id:int = Path(title="object id")):
+    for name in names:
+        if name['id'] == name_id:
+            return name
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+
+
+@app.post("/names",status_code=status.HTTP_201_CREATED)
+def creat_name(name : str = Form()):
+    name_obj={"id":random.randint(6,100),"name":name}
+    names.append(name_obj)
+    return name_obj
+
+
+@app.put("/names/{name_id}")
+def update_name(name_id:int = Path(),name:str=Query()):
+    for item in names:
+        if item['id']==name_id:
+            item['name'] = name
+            return name
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+
+
+@app.delete("/names/{name_id}")
+def delete_name(name_id:int):
+    for item in names:
+        if item['id'] == name_id:
+            names.remove(item)
+            return {"message":"item deleted"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")    
+
+
+@app.get("/")
+def root():
+    return JSONResponse(content={"message":"server testing"},status_code=status.HTTP_202_ACCEPTED)
+
+@app.post("/submit")
+def submit_form(username :str = Form(... , alias="username"), password:str=Form(... , alias="password")):
+    return {"username": username, "password": password}
