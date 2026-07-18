cf70db995bf6fd4a92404379bd61237f88c30c6a
Hamidreza Bakhshi <hamidrezabakhshi8118@gmail.com>
2026-07-18 13:59:54 +0330

    feat:update crud

--- DataBase.py
+++ DataBase.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine , Column , Integer , String , BOOLEAN ,DateTime
-from sqlalchemy.orm import sessionmaker , declarative_base 
+from sqlalchemy import create_engine , Column , Integer , String , BOOLEAN ,DateTime , ForeignKey
+from sqlalchemy.orm import sessionmaker , declarative_base , relationship
 
 
 DATABASE_URL="postgresql://postgres:147852@localhost:5432/fastapi_course"
@@ -23,6 +23,24 @@ class User(Base):
     is_active= Column(BOOLEAN,default=True)
     joined_at = Column(DateTime)
 
+    addresses = relationship("Addres", back_populates="user")
 
+
+    def __repr__(self):
+        return f"User(id={self.id}, first_name='{self.first_name}', last_name='{self.last_name}')"
+
+class Addres(Base):
+    __tablename__= "addres"
+
+    id = Column(Integer , primary_key=True , autoincrement=True)
+    user_id = Column(Integer , ForeignKey("users.id"))
+    city = Column(String)
+    state = Column(String)
+    zip_code = Column(String)
+
+    user = relationship("User", back_populates="addresses")
+
+    def __repr__(self):
+        return f"Addres(id={self.id}, city='{self.city}', state='{self.state}', zip_code='{self.zip_code}')"
 
 Base.metadata.create_all(engine)
--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
-from DataBase import User , SessionLocal
+from DataBase import User , SessionLocal , Addres
 from datetime import datetime
 from sqlalchemy import select ,func
+from sqlalchemy.sql import text
 session=SessionLocal()
 
 
@@ -20,5 +21,33 @@ session=SessionLocal()
 #user = session.query(User).where(User.first_name.like("ali")).all()
 
 
-user = session.query(func.max(User.age)).scalar()
-print(user)
+#user = session.query(func.max(User.age)).scalar()
+#print(user)
+
+#query = text("SELECT * FROM  users")
+#result=session.execute(query).all()
+#print(result)
+
+
+#address_1 = Addres(city="karaj",state="Alborz",zip_code="2002")
+#address_2 = Addres(city="tehran",state="tehran",zip_code="3003")
+#address_3 = Addres(city="mashhad",state="khorasan_razavi",zip_code="4004")
+
+#all_addresses = [address_1,address_2,address_3]
+
+#session.add_all(all_addresses)
+#session.commit()
+
+#user = session.query(User).where(User.first_name.like("ali")).one_or_none()
+
+#address = session.query(Addres).where(Addres.user_id==user.id).all()
+#print(address)
+
+
+
+#result = session.query(User).join(User.addresses).where(User.first_name == "ali").all()
+#print(result)
+
+
+users = session.query(User).join(User.addresses).where(Addres.city == "tehran").all()
+print(users)
